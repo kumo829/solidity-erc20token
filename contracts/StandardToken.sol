19ce1c111a6e13b74b20817b04825ead50dc4f28
Alex Montoya <amonmar2000@hotmail.com>
2018-07-10 06:53:42 -0500

    corrected typo from StandardToken in import statement

--- contracts/StandardToken.sol
+++ contracts/StandardToken.sol
@@ -1,7 +1,34 @@
 pragma solidity ^0.4.24;
 
-import "../contract/ERC20Interface.sol";
+import "../contracts/ERC20Interface.sol";
 
-contract StandardToken is ERC20TokenInterface{
+contract StandardToken is ERC20Interface{
+    mapping (address => uint256) balances;
+    mapping (address => mapping (address => uint256)) allowed;
+    uint256 public totalSupply;
 
+    function balanceOf(address tokenOwner) public view returns (uint balance){
+        return balances[tokenOwner];
+    }
+
+    function allowance(address tokenOwner, address spender) public view returns (uint remaining) {
+        return allowed[tokenOwner][spender];
+    }
+
+    function transfer(address to, uint tokens) public returns (bool success) {
+        //burning ether
+        require(to != 0x0);
+
+        //validamos overflow
+        require(balances[to] + tokens >= balances[to]);
+
+        //validamos que tenga suficiente saldo y tokens
+        require(balances[msg.sender] >= tokens && tokens > 0);
+
+        balances[msg.sender] -= tokens;
+        balances[to] += tokens;
+
+        emit Transfer(msg.sender, to, tokens);
+        return true;
+    }
 }
