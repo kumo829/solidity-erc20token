452031599a2560a008e1800b552e877ba27f8e6c
Alex Montoya <amonmar2000@hotmail.com>
2018-07-29 14:19:00 -0500

    removed index of account and added account address to avoid warning

--- private_network/start-network.bat
+++ private_network/start-network.bat
@@ -1,1 +1,1 @@
-geth --datadir .\chaindata --rpc --rpccorsdomain "*" --rpcapi "db,eth,net,web3,personal" --unlock 0 --mine --minerthreads 1
+geth --datadir .\chaindata --rpc --rpccorsdomain "*" --rpcapi "db,eth,net,web3,personal" --unlock b6e08874e9f1658ccffe0eef47f830b34dc7f006 --mine --minerthreads 1 --nodiscover 
